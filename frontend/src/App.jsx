b7ca30843cc161c3e4b03e45cf62a6f93979f578
axile24 <amokrane.n@icloud.com>
2025-12-08 21:16:50 +0100

    Frontend: Lägg till huvudkomponenter och styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import BookingPage from './pages/BookingPage'
+import ConfirmationPage from './pages/ConfirmationPage'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>🎳 Strajk Bowling</h1>
+        <p>Boka din tid i centrala Bromölla</p>
+      </header>
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<BookingPage />} />
+          <Route path="/bekraftelse" element={<ConfirmationPage />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
+
